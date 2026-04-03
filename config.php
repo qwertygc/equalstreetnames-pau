2366ad59c441bf4dca3b8d145dd840fa9fa2f8c2
Qwerty <qwerty@legtux.org>
2026-04-03 09:03:58 +0200

    Update config.php

--- config.php
+++ config.php
@@ -7,7 +7,7 @@ return [
     'relationId' => 162431,
 
     // Languages in which you want to extract Wiki informations
-    'languages' => ['en'],
+    'languages' => ['fr','en'],
 
     // Exclude datas from beeing extracted. Usefull to exclude streets at the edge of the city for example
     'exclude' => [
